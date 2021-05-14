30c582a4b32c023ac7efcf7c5f3ac1b5f6249d8f
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2021-05-14 11:47:22 +0900

    Add manifest link

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,9 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="ja">
-        <Head />
+        <Head>
+          <link rel="manifest" href="/site.webmanifest" />
+        </Head>
         <body>
           <Main />
           <NextScript />
