e2b142eff4236176670761ddc190634a2e997b0c
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2021-08-07 13:01:14 +0900

    Support Firebase v8

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,8 @@ import React from "react";
 import { useEffect } from "react";
 import "firebase/firestore";
 import "firebase/auth";
-import { Fuego, FuegoProvider } from "@nandorojo/swr-firestore";
+import { Fuego } from "../lib/fuego";
+import { FuegoProvider } from "@nandorojo/swr-firestore";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY,
