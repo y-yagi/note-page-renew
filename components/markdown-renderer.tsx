8fbf1280559081466152b14d6be476d7061f5e3b
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2022-02-11 16:35:38 +0900

    Set `target="_blank"` by default

--- components/markdown-renderer.tsx
+++ components/markdown-renderer.tsx
@@ -7,12 +7,31 @@ interface Props {
 }
 
 const MarkdownRenderer = ({ content }: Props) => {
+  // NOTE: see https://github.com/markedjs/marked/issues/655
+  const renderer = new marked.Renderer();
+  const linkRenderer = renderer.link;
+  renderer.link = (href, title, text) => {
+    console.info(href);
+    const localLink = href?.startsWith(
+      `${location.protocol}//${location.hostname}`
+    );
+    const html = linkRenderer.call(renderer, href, title, text);
+    return localLink
+      ? html
+      : html.replace(
+          /^<a /,
+          `<a target="_blank" rel="noreferrer noopener nofollow" `
+        );
+  };
+
   marked.setOptions({ breaks: true });
 
   return (
     <div
       dangerouslySetInnerHTML={{
-        __html: DOMPurify.sanitize(marked(content)),
+        __html: DOMPurify.sanitize(marked(content, { renderer }), {
+          ADD_ATTR: ["target"],
+        }),
       }}
     ></div>
   );
